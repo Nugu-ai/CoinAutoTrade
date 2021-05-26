668ec499faa800de7e89ddd3235f60b68f369080
Nugu-ai <84832167+Nugu-ai@users.noreply.github.com>
2021-05-26 16:14:38 +0900

    Add files via upload

---
+++ MYXRPAutoTradeAI.py
@@ -0,0 +1,107 @@
+import time
+import pyupbit
+import datetime
+import schedule
+import numpy as np
+from fbprophet import Prophet
+
+access = "hnijp8VO0cC5Wx09dD2tgyJIeaWfcpqbFxYGj3z8"
+secret = "rNP8SbOZwdHVRQIY60WaWIDDnosFSvIGWqSdeUwu"
+
+def get_target_price(ticker, k):
+    """변동성 돌파 전략으로 매수 목표가 조회"""
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
+    target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
+    return target_price
+
+def get_start_time(ticker):
+    """시작 시간 조회"""
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=1)
+    start_time = df.index[0]
+    return start_time
+
+def get_balance(ticker):
+    """잔고 조회"""
+    balances = upbit.get_balances()
+    for b in balances:
+        if b['currency'] == ticker:
+            if b['balance'] is not None:
+                return float(b['balance'])
+            else:
+                return 0
+    return 0
+
+def get_current_price(ticker):
+    """현재가 조회"""
+    return pyupbit.get_orderbook(tickers=ticker)[0]["orderbook_units"][0]["ask_price"]
+
+predicted_close_price = 0
+def predict_price(ticker):
+    """Prophet으로 당일 종가 가격 예측"""
+    global predicted_close_price
+    df = pyupbit.get_ohlcv(ticker, interval="minute60")
+    df = df.reset_index()
+    df['ds'] = df['index']
+    df['y'] = df['close']
+    data = df[['ds','y']]
+    model = Prophet()
+    model.fit(data)
+    future = model.make_future_dataframe(periods=24, freq='H')
+    forecast = model.predict(future)
+    closeDf = forecast[forecast['ds'] == forecast.iloc[-1]['ds'].replace(hour=9)]
+    if len(closeDf) == 0:
+        closeDf = forecast[forecast['ds'] == data.iloc[-1]['ds'].replace(hour=9)]
+    closeValue = closeDf['yhat'].values[0]
+    predicted_close_price = closeValue
+predict_price("KRW-XRP")
+schedule.every().hour.do(lambda: predict_price("KRW-XRP"))
+
+def get_ror(k, ticker):
+    df = pyupbit.get_ohlcv(ticker, count=7)
+    df['range'] = (df['high'] - df['low']) * k
+    df['target'] = df['open'] + df['range'].shift(1)
+
+    df['ror'] = np.where(df['high'] > df['target'],
+                         df['close'] / df['target'],
+                         1)
+
+    ror = df['ror'].cumprod()[-2]
+    return ror
+
+def find_bestk(ticker):
+    global bestk
+    k_ls = []
+    for k in np.arange(0.1, 1.0, 0.1):
+        ror = get_ror(k, ticker)
+        k_ls.append([k,ror])
+
+    bestk = max(k_ls, key=lambda x: x[1])[0]
+schedule.every().day.at("9:00").do(find_bestk("KRW-XRP"))
+
+# 로그인
+upbit = pyupbit.Upbit(access, secret)
+print("autotrade start")
+
+# 자동매매 시작
+while True:
+    try:
+        now = datetime.datetime.now()
+        start_time = get_start_time("KRW-XRP")
+        end_time = start_time + datetime.timedelta(days=1)
+        schedule.run_pending()
+
+        if start_time < now < end_time - datetime.timedelta(seconds=10):
+            target_price = get_target_price("KRW-XRP", bestk)
+            current_price = get_current_price("KRW-XRP")
+            if target_price < current_price and current_price < predicted_close_price:
+                krw = get_balance("KRW")
+                if krw > 5000:
+                    upbit.buy_market_order("KRW-XRP", krw*0.9995)
+        else:
+            btc = get_balance("XRP")
+            if btc > 5000 / current_price:
+                upbit.sell_market_order("KRW-XRP", btc*0.9995)
+        time.sleep(1)
+    except Exception as e:
+        print(e)
+        time.sleep(1)
